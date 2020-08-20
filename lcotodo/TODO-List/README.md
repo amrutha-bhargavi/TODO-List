31d40f99ccbb9e38603e75c78497cab7d00c5c21
amrutha-bhargavi <62816032+amrutha-bhargavi@users.noreply.github.com>
2020-08-20 16:40:50 -0500

    Add files via upload

---
+++ lcotodo/TODO-List/README.md
@@ -0,0 +1,2 @@
+# TODO-List
+A web interface for a TODO list - (ReactJS basics)
---
+++ lcotodo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg'
+
+function App() {
+  return(
+    <div className="App">
+      <h1>Amrutha</h1>
+      <h1>Rajkumar</h1>
+    </div>
+  );
+}
+
+export default App;
